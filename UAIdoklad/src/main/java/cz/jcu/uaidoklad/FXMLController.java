a4f9df884fb44a8a7cf683b7a5615a54e4c51a3e
Tom4ss <stveracek.t@seznam.cz>
2018-06-26 19:05:24 +0200

    Pridal jsem uvodni okno

--- UAIdoklad/src/main/java/cz/jcu/uaidoklad/FXMLController.java
+++ UAIdoklad/src/main/java/cz/jcu/uaidoklad/FXMLController.java
@@ -5,14 +5,22 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private Button PrihlasitBtn;
+    @FXML
+    private Button RegistrovatBtn;
+    @FXML
+    private TextField loginTextField;
+    @FXML
+    private TextField HesloTextField;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -22,4 +30,12 @@ public class FXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void PrihlasitClickedBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void RegistrovatClickedBtn(ActionEvent event) {
+    }
 }
