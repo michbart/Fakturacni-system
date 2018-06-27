fa52718347b924054c92cc9d76200e34ab315856
Petr Zazvorka <sew21.skola@gmail.com>
2018-06-27 08:58:26 +0200

    ulozeni + nacteni faktury do souboru

--- UAIdoklad/src/main/java/cz/jcu/uaidoklad/Controller/Soubory.java
+++ UAIdoklad/src/main/java/cz/jcu/uaidoklad/Controller/Soubory.java
@@ -8,8 +8,84 @@ package cz.jcu.uaidoklad.Controller;
 
 /**
  *
- * @author Michal
+ * @author Petr
  */
 public class Soubory {
+    
+    private String cestaKSouborum;
+    
+    
+    
+    /**
+     * 
+     * @param cestaKSouborum
+     * @throws Exception 
+     */
+    
+    public Soubor(String cestaKSouborum) throws Exception {
+        File soubor = new File(cesta);
+        if (!soubor.isDirectory()){
+            if soubor.mkdirs()){
+                throw new Exception("Nastala chyba pri tvorbe adresare");
+            }
+        }
+    }
+    
+    
+    
+  
+ /**
+  * 
+  * @param cesta
+  * @throws Exception 
+  */
+    
+    private void overeniCesty(String cesta) throws Exception {
+        File soubor = new File(cesta);
+        if (!soubor.isDirectory()) {
+            if (!soubor.mkdirs()) {
+                throw new Exception("Nastala chyba pri tvorbe adresare);
+            }
+        }
+    }
+    
+    
+
+    /**
+     * 
+     * @param faktura
+     * @throws Exception 
+     */
+    
+    
+    void ulozeniFaktury(Faktura faktura) throws Exception {
+        
+   // TO DO: zatim neznam parametr faktury pri ukladani -> faktura.ziskatNazev();
+   
+    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(cestaKSouborum + faktura.ziskatNazev() + ".dat")));) {
+            oos.writeObject(faktura);
+        } catch (Exception e) {
+            throw new Exception("Nastala chyba pri ukladani faktury do souboru.");
+        }
+    }
+
+
+
+
+/**
+ *  TO DO: opet parametr nazevFaktury
+ */
+
+    Faktura nacistFakturu(String nazevFaktury) throws Exception {
+        Faktura faktura;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(cestaKSouborum + nazevFaktury + ".dat")));) {
+            faktura = (Faktura) ois.readObject();
+        } catch (Exception ex) {
+            throw new Exception("Nastala chyba nacitani faktury ze souboru.");
+        }
+        return faktura;
+    }
+
+    
     
 }
