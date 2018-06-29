16f7f04fc87477d700a0a52a2453c849102a9b97
Nikola Kraus <nikolakraus@seznam.cz>
2018-06-29 08:47:55 +0200

    uprava Firmamock

--- src/main/java/cz/jcu/uaidoklad/Model/FirmaRepositoryMock.java
+++ src/main/java/cz/jcu/uaidoklad/Model/FirmaRepositoryMock.java
@@ -35,10 +35,10 @@ public class FirmaRepositoryMock implements FirmaRepository {
         polozky = new ArrayList();
         vytvorFirmy();
         pridejFirmy();
-        vytvorFaktury();
-        pridejFaktury();
         vytvorPolozky();
         pridejPolozky();
+        vytvorFaktury();
+        pridejFaktury();
     }
 
     @Override
@@ -47,16 +47,17 @@ public class FirmaRepositoryMock implements FirmaRepository {
     }
 
     private void vytvorFirmy() {
-        f1 = new Firma(1, "ENGEL strojírenská spol. s r.o.", "Českobudějovická", 15319, "Kaplice", "Česká republika", 62497219, 456, "1234567890", "email@email.cz", "1234567/0300");
+        f1 = new Firma(1, "ENGEL strojírenska spol. s r.o.", "Ceskobudejovicka", 15319, "Kaplice", "Ceska republika", 62497219, 456, "1234567890", "email@email.cz", "1234567/0300");
         f2 = new Firma(2, "nazev1", "ulice", 123, "mesto1", "stat1", 123, 456, "1111111", "email1", "123456789/0900");
         f3 = new Firma(3, "nazev2", "ulice2", 456, "mesto2", "stat2", 123, 456, "1111111", "email2", "123456789/0900");
         f4 = new Firma(4, "nazev3", "ulice3", 789, "mesto3", "stat3", 123, 456, "1111111", "email3", "123456789/0900");
         f5 = new Firma(5, "nazev4", "ulice4", 135, "mesto4", "stat4", 123, 456, "1111111", "email4", "123456789/0900");
     }
-    public String getNazev(Firma f){
+
+    public String getNazev(Firma f) {
         return f.getNazev();
     }
-    
+
     private void pridejFirmy() {
         firmy.add(f1);
         firmy.add(f2);
@@ -67,24 +68,24 @@ public class FirmaRepositoryMock implements FirmaRepository {
 
     private void vytvorFaktury() {
         HashMap<Integer, Integer> polozkyList = new HashMap();
-        for (int i = 0; i < 10; i++) {
-            polozkyList.put(i, i + 10);
-        }
+        polozkyList.put(p1.getId(), 23);
+        polozkyList.put(p2.getId(), 12);
+        polozkyList.put(p3.getId(), 18);
         fa1 = new Faktura(1, 1234, f1, f2, polozkyList, "1.2.2111", "hotově Vatikanskou měnou", 1);
         fa2 = new Faktura(2, 5678, f1, f3, polozkyList, "32.13.0000", "qwert", 2);
     }
 
     private void pridejFaktury() {
-        System.out.println("tu"+fa1.getId());
+        System.out.println("tu" + fa1.getId());
         faktury.add(fa1);
         faktury.add(fa2);
     }
-    
+
     private void vytvorPolozky() {
-        
-        p1 = new Polozka(1,"banan",120.234,"t");
-        p2 = new Polozka(2,"qwert",12.24,"qwer");
-        p3 = new Polozka(3,"pomerač",10.34,"l");
+
+        p1 = new Polozka(1, "banan", 120.234, "t");
+        p2 = new Polozka(2, "qwert", 12.24, "qwer");
+        p3 = new Polozka(3, "pomerač", 10.34, "l");
     }
 
     private void pridejPolozky() {
@@ -92,4 +93,5 @@ public class FirmaRepositoryMock implements FirmaRepository {
         polozky.add(p2);
         polozky.add(p3);
     }
+
 }
